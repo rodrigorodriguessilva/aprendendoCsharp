3feaca257c5dc51862402f2b6ca28af8d552d139
Rodrigo Rodrigues Silva <rodrigo2rodrigues@outlook.com>
2024-08-06 16:29:28 -0300

    Adicionado Projeto 2

---
+++ Cap02_Ex01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap02_Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
