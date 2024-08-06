444441c181c6a9f7181708682067723cfa7efb66
Rodrigo Rodrigues Silva <rodrigo2rodrigues@outlook.com>
2024-08-06 16:26:18 -0300

    Alterado main acrescentado segunda linha

--- aprendendocsharp/Program.cs
+++ aprendendocsharp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace aprendendocsharp
 {
@@ -11,6 +7,8 @@ namespace aprendendocsharp
         static void Main(string[] args)
         {
             Console.WriteLine("Ola Mundo");
+            Console.WriteLine("  dsd  ");
+
         }
     }
 }
